c6a5bab97b823c877fe60bc7a30933c9dca7a18a
h.yosefpour <hesam.init@gmail.com>
2022-05-01 13:47:32 +0430

    [UPDATE] : get data using loop

--- app.js
+++ app.js
@@ -1,36 +1,48 @@
 import cheerio from "cheerio";
 import pretty from "pretty";
 import axios from "axios";
+import colors from "colors";
+import fs from "fs";
 
 // scraper function
 const url = "https://hidemy.name/en/proxy-list/";
 
 const scrapeData = () => {
-  axios
-    .get(url)
-    .then((data) => {
-      const $ = cheerio.load(data.data);
-      const allProxies = $(".pagination > ul");
-      const getPagination = allProxies.children().length - 1;
-      const getAllCountProxies = $(
-        `.pagination > ul > li:nth-child(${getPagination}) > a`
-      );
-      const getCountProxies = getAllCountProxies
-        .attr("href")
-        .split("=")
-        .pop()
-        .split("#")[0];
+  axios.get(url).then((data) => {
+    const $ = cheerio.load(data.data);
+    const allProxies = $(".pagination > ul");
+    const getPagination = allProxies.children().length - 1;
+    const getAllCountProxies = $(
+      `.pagination > ul > li:nth-child(${getPagination}) > a`
+    );
+    const getCountProxies = getAllCountProxies
+      .attr("href")
+      .split("=")
+      .pop()
+      .split("#")[0];
 
-      // get url in loop
-      const urlGenerated = [];
-      for (let i = 64; i < parseInt(getCountProxies); i += 64) {
-        urlGenerated.push(`https://hidemy.name/en/proxy-list/?start=${i}#list`);
-      }
-      console.log(urlGenerated);
-    })
-    .catch((err) => {
-      console.log(err);
+    // get url in loop
+    const urlGenerated = [];
+    for (let i = 64; i < parseInt(getCountProxies); i += 64) {
+      urlGenerated.push(`https://hidemy.name/en/proxy-list/?start=${i}#list`);
+    }
+
+    urlGenerated.forEach((data, index) => {
+      setTimeout(() => {
+        axios.get(urlGenerated[index]).then((data) => {
+          const $ = cheerio.load(data.data);
+          const listItems = $(".table_block tbody").html();
+          fs.writeFile(`./export/${index}.html`, pretty(listItems), (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log(`File ${index} exported`.green);
+          });
+        });
+      }, index * 1000);
     });
+  });
 };
 
 scrapeData();
